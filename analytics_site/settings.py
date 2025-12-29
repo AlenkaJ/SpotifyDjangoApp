46efa5fc0d96565112113003144911b72eac17b2
AlenkaJ <aja.jarolimova@seznam.cz>
2025-12-29 15:55:55 +0100

    add security stuff

--- analytics_site/settings.py
+++ analytics_site/settings.py
@@ -181,3 +181,8 @@ LOGOUT_URL = "/spotify_filter/logout/"
 LOGOUT_REDIRECT_URL = "/spotify_filter/"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
